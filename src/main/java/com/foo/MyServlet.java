8b9ea54bc24e8d9e685b61ad4468442ee454d1ec
Joseph Perez <jdperez@utexas.edu>
2012-06-15 16:27:50 -0500

    created toy servlet and modified xml files so it may run on server

--- src/main/java/com/foo/MyServlet.java
+++ src/main/java/com/foo/MyServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.Math;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,8 +14,10 @@ import java.io.IOException;
  * Time: 2:00 PM
  * To change this template use File | Settings | File Templates.
  */
+
 public class MyServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print("hello,world");
+        resp.getWriter().print("hello,world!");
+
     }
 }
