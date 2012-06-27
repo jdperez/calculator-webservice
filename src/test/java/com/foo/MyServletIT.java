a8cbd0d5d8ed4b2ba1ade710ffca2b019bc286ce
Joseph Perez <joseph.perez@rackspace.com>
2012-06-27 11:50:51 -0500

    Added helper functions to MyServletIT to simplify the construction of http requests and responses

--- src/test/java/com/foo/MyServletIT.java
+++ src/test/java/com/foo/MyServletIT.java
@@ -3,15 +3,19 @@ package com.foo;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -25,50 +29,58 @@ import static org.junit.Assert.assertThat;
  * To change this template use File | Settings | File Templates.
  */
 public class MyServletIT {
-    DefaultHttpClient httpClient = new DefaultHttpClient();
+    RequestAndResponse requestAndResponse;
 
     @Test
     public void successfulPost() throws Exception{
-        List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
-        nameValuePairList.add(new BasicNameValuePair("operand1", "2"));
-        nameValuePairList.add(new BasicNameValuePair("operand2", "2"));
-        nameValuePairList.add(new BasicNameValuePair("operator", "ADD"));
-        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(nameValuePairList, "UTF-8");
-        HttpPost httpPost = new HttpPost("http://localhost:8888/foo");
-        httpPost.setEntity(entity);
-        HttpResponse response = httpClient.execute(httpPost);
-        HttpEntity httpEntity = response.getEntity();
+        requestAndResponse =
+                doPost("http://localhost:8888/foo",
+                        param("operand1", "2"), param("operand2", "2"), param("operator", "ADD"));
+        HttpEntity httpEntity = requestAndResponse.response.getEntity();
         assertThat(EntityUtils.toString(httpEntity),equalTo("4"));
-        httpPost.releaseConnection();
+        requestAndResponse.request.releaseConnection();
     }
-
+  /*
     @Test
     public void initializeDatabaseSuccessful() throws Exception {
-        List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
-        nameValuePairList.add(new BasicNameValuePair("operand1", "40"));
-        nameValuePairList.add(new BasicNameValuePair("operand2", "2"));
-        nameValuePairList.add(new BasicNameValuePair("operator", "ADD"));
-        HttpPost httpPost = new HttpPost("http://localhost:8888/foo");
-        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(nameValuePairList, "UTF-8");
-        httpPost.setEntity(entity);
-        httpClient.execute(httpPost);
-        List<NameValuePair> historyNameValuePairList = new ArrayList<NameValuePair>();
-        nameValuePairList.add(new BasicNameValuePair("operation", "HISTORY"));
-        UrlEncodedFormEntity historyEntity = new UrlEncodedFormEntity(historyNameValuePairList, "UTF-8");
-        httpPost.setEntity(historyEntity);
-        HttpResponse httpResponse =  httpClient.execute(httpPost);
-        HttpEntity httpEntity = httpResponse.getEntity();
+        requestAndResponse = doPost("http://localhost:8888/foo",
+                                    param("operand1","40"), param("operand2","2"),param("operator","ADD"));
+        requestAndResponse.request.releaseConnection();
+        requestAndResponse = doPost("http://localhost:8888/foo",param("operator","HISTORY"));
+        HttpEntity httpEntity = requestAndResponse.response.getEntity();
         assertThat(EntityUtils.toString(httpEntity), equalTo("40 ADD 2"));
+        requestAndResponse.request.releaseConnection();
+    }
+ */
+
+
+    private NameValuePair param(String key, String value) {
+        return new BasicNameValuePair(key, value);
     }
 
+    private RequestAndResponse doPost(String uri, NameValuePair... params) throws IOException {
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(Arrays.asList(params), "UTF-8");
+        HttpPost httpPost = new HttpPost(uri);
+        httpPost.setEntity(entity);
+        HttpResponse response = httpClient.execute(httpPost);
+        return new RequestAndResponse(httpPost, response);
+    }
 
+    private class RequestAndResponse {
+        public final HttpRequestBase request;
+        public final HttpResponse response;
 
+        public RequestAndResponse(HttpRequestBase request, HttpResponse response) {
+            this.request = request;
+            this.response = response;
+        }
+    }
 
 
 /** Mocking servlet container with ServletUnit
-
     @Test
-    public void successfulPost() throws Exception{
+    public void successfulPost() throws Exception {
         File file = new File("/home/jose5124/dev/projects/project1/src/main/webapp/WEB-INF/web.xml");
         ServletRunner servletRunner = new ServletRunner(file);
         servletRunner.registerServlet("myServlet", MyServlet.class.getName());
@@ -84,5 +96,6 @@ public class MyServletIT {
         assertThat(webResponse.getContentType(),equalTo("text/html"));
         assertThat(webResponse.getText(), equalTo("4"));
     }
-*/
+**/
+
 }
