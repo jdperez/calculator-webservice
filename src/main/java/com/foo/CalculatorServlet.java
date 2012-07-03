114463bcf56f3df56ef6e88d4397e0ab0eaf9f20
Joseph Perez <joseph.perez@rackspace.com>
2012-07-03 17:07:45 -0500

    unused import

--- src/main/java/com/foo/CalculatorServlet.java
+++ src/main/java/com/foo/CalculatorServlet.java
@@ -1,7 +1,6 @@
 package com.foo;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletConfig;
