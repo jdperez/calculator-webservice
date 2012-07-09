ffd9419fd2d1ca211cf0e01f954841f96394ff28
Joseph Perez <joseph.perez@rackspace.com>
2012-07-09 09:48:38 -0500

    created exception for user input and mapper to send response

---
+++ src/main/java/com/foo/BadUserInputException.java
@@ -0,0 +1,11 @@
+package com.foo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jose5124
+ * Date: 7/6/12
+ * Time: 4:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BadUserInputException {
+}
---
+++ src/main/java/com/foo/CalculatorJaxRsResource.java
@@ -0,0 +1,11 @@
+package com.foo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jose5124
+ * Date: 7/6/12
+ * Time: 3:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CalculatorJaxRsResource {
+}
